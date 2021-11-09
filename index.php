1e3c4125c3809fd7b5c75112d15fde5eea1bc958
KaloyJS <carlojs@protonmail.com>
2021-11-08 20:46:46 -0500

    first commit.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$application = new \App\Helpers\App();
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+echo $application->isRunningFromConsole() . PHP_EOL;
---
+++ src/Helpers/Utility.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+class Utility
+{
+    public static function printArr($arr)
+    {
+        echo "<pre>";
+        print_r($arr);
+        echo "</pre>";
+    }
+}
